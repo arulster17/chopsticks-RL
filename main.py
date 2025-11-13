4a7b30c6c8181d74c2cb2812d66a0e18b64df135
Arul Mathur <arulster17@gmail.com>
2025-11-12 21:02:33 -0800

    added generation for all states + initial table

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import itertools
+
 # all the program stuff here
 
 numplayers = 2 # number of players
-maxturns = 100 # maximum turns
+maxturns = 100 # maximum turns, maximum move is maxturns-1 so the 100 moves are 0 to 99
+numgames = 10000 # numgames
 
 # state will be n+1 length list where first n are 2d tuples
 # for n = 2, will be [(p1 LH, p1 RH), (p2 LH, p2 RH), moveCounter]
@@ -80,8 +83,21 @@ def stateactions(currentstate):
         # if any entry is geq 5, make it 0 and normalize
 
         (c,d) = currentstate[i]
-        # could precompute this
-        candidate_atks = {normalize((a+c,d)), normalize((b+c,d)), normalize((c,a+d)), normalize((c,b+d))}
+
+        # could precompute this or maybe at least write this better lol
+        candidate_atks = set()
+        if a != 0:
+            if c != 0:
+                candidate_atks.add(normalize((a+c,d)))
+            if d != 0:
+                candidate_atks.add(normalize((c,a+d)))
+        if b != 0:
+            if c != 0:
+                candidate_atks.add(normalize((b+c,d)))
+            if d != 0:
+                candidate_atks.add(normalize((c,b+d)))
+
+        
         for cand in candidate_atks:
             newstate = currentstate.copy()
             newstate[i] = cand
@@ -89,5 +105,40 @@ def stateactions(currentstate):
 
     return newstatelist
 
+# compute a dictionary of all states to actions+weights
+# keys: states
+# values: pairs of (newstates, weights)
+# initialize this to 1
 
-print(stateactions([(1,1),(1,1),0]))
+# generate a list of all states using itertools
+# for n players we want the cartesian product of n players and the move counter
+
+all_hands = VALID_SWAPS.keys() # lazy trick lol
+all_move_counters = list(range(0, maxturns))
+lists = [all_hands]*numplayers + [all_move_counters]
+
+# cartesian product
+product_iter = itertools.product(*lists)
+all_states = [list(t) for t in product_iter]
+
+print(len(all_states))
+
+# create q table
+# need to convert states from list to tuples because you cannot hash lists
+qtable = {}
+for state in all_states: # for all states
+    state_tuple = tuple(state)
+    qtable[state_tuple] = {} # initialize weight dict
+    next_states = stateactions(state) # get all next states
+    statemap = qtable[state_tuple]
+    for ns in next_states: # for all next states
+        statemap[tuple(ns)] = 1 # set default weight to 1
+
+
+
+# test [(1,1),(1,3),0]
+d = qtable[tuple([(1,1),(1,3),0])]
+for entry in d:
+    print(str(entry) + ": " + str(d[entry]))
+
+#print(stateactions([(1,1),(1,0),0]))
